127e512966a8929307a9594ad63cd8929da14033
Alifna Nur Fadhilla <alifnanur05@gmail.com>
2025-05-09 19:15:19 +0700

    membuat file h_pengguna

--- admin/h_pengguna.php
+++ admin/h_pengguna.php
@@ -6,10 +6,10 @@ if (isset($_GET['id'])) {
     $id_user = $_GET['id'];
     
     //Query untuk menghapus data berdasarkan id_user
-    $query = mysqli_query($koneksi, "DELETE FROM tb_user WHERE id_user = '$id_user'");
+    $delete = mysqli_query($koneksi, "DELETE FROM tb_user WHERE id_user = '$id_user'");
 
     //Notifikasi dan redirect
-    if ($query) {
+    if ($delete) {
         echo "<script>alert('Data pengguna berhasil dihapus!')</script>";
         header("refresh:0, pengguna.php");
     } else {
