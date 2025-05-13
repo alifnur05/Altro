7af95b8c02f743b6fd518b9c273cbf56566554e0
Alifna Nur Fadhilla <alifnanur05@gmail.com>
2025-05-13 19:06:04 +0700

    membuat file index

--- admin/index.php
+++ admin/index.php
@@ -189,8 +189,8 @@ if (!isset($_SESSION["status"]) || $_SESSION["status"] !== "admin") {
             <div class="col-12">
               <div class="card info-card customers-card shadow-sm w-100">
                 <div class="card-body text-center py-4">
-                  <h4 class="mb-2"> Selamat Datang di Website Admin <strong>Altro</strong></h4>
-                  <p class="text-muted small mb-0">Kelola produk, transaksi, dan pelanggan dengan mudah</p>
+                  <h4 class="mb-2"> Welcome to Website Admin <strong>Altro</strong></h4>
+                  <p class="text-muted small mb-0">Hanya menjual peralatan elektronik, bukan jual perasaan.</p>
                 </div>
               </div>
             </div><!-- End Welcome Card -->
@@ -200,14 +200,14 @@ if (!isset($_SESSION["status"]) || $_SESSION["status"] !== "admin") {
               <div class="card info-card sales-card">
 
                 <div class="card-body">
-                  <h5 class="card-title">Pesanan <span>| Semua waktu</span></h5>
+                  <h5 class="card-title">Pesanan <span>| Minggu ini</span></h5>
 
                   <div class="d-flex align-items-center">
                     <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
                       <i class="bi bi-basket"></i>
                     </div>
                     <div class="ps-3">
-                      <h6>145</h6>
+                      <h6>14500</h6>
                     </div>
                   </div>
                 </div>
@@ -226,7 +226,7 @@ if (!isset($_SESSION["status"]) || $_SESSION["status"] !== "admin") {
                       <i class="bi bi-currency-dollar"></i>
                     </div>
                     <div class="ps-3">
-                      <h6>Rp. 32.264</h6>
+                      <h6>Rp. 15.932.264</h6>
                     </div>
                   </div>
                 </div>
